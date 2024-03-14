2292b8cc8da94f16c30c01f32206d624480ae45d
wongeunsong <wongeunsong94@gmail.com>
2024-03-14 13:06:39 +0900

    feat: change log format

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -1,18 +1,21 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction, response } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   private readonly logger = new Logger();
 
   use(req: Request, res: Response, next: NextFunction) {
-    const { ip, method, originalUrl, headers, body } = req;
+    const { ip, method, originalUrl, headers, params, query, body } = req;
     const reqHeader = JSON.stringify(headers);
     const reqBody = JSON.stringify(body);
+    const reqParmas = JSON.stringify(params);
+    const reqQuery = JSON.stringify(query);
+
     res.on('finish', () => {
       const { statusCode } = res;
       this.logger.log(
-        `${method} ${originalUrl} ${statusCode} ${ip} \nheaders: ${reqHeader},\nbody: ${reqBody}`,
+        `${method} ${originalUrl} ${statusCode} ${ip} \n[Request] headers: ${reqHeader}, params: ${reqParmas}, query: ${reqQuery}, body: ${reqBody}`,
       );
     });
 
